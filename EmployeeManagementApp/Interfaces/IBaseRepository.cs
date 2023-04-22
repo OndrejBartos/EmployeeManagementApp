2409e87c5e2667329f80b31f2f09be6f348e246e
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 20:24:42 +0200

    Create IBaseRepository and BaseRepository

---
+++ EmployeeManagementApp/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagementAPI.Interfaces;
+
+public interface IBaseRepository<T> where T : IModel
+{
+    public ICollection<T> GetAll();
+    public T? GetById(int id);
+    public bool Create(T entity);
+    public bool Update(T entity);
+    public bool Delete(int id);
+    public bool Exists(int id);
+}
---
+++ EmployeeManagementApp/Repositories/BaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using EmployeeManagementAPI.Data;
+using EmployeeManagementAPI.Interfaces;
+
+namespace EmployeeManagementAPI.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class, IModel
+{
+    protected readonly DataContext _context;
+
+    protected BaseRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public bool Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        return Save();
+    }
+
+    public bool Delete(int id)
+    {
+        var entity = GetById(id);
+
+        if (entity is null)
+            return false;
+
+        _context.Set<T>().Remove(entity);
+        return Save();
+    }
+
+    public bool Exists(int id)
+    {
+        return _context.Set<T>().Any(entity => entity.Id == id);
+    }
+
+    public ICollection<T> GetAll()
+    {
+        return _context.Set<T>().ToArray();
+    }
+
+    public T? GetById(int id)
+    {
+        return _context.Set<T>().FirstOrDefault(entity => entity.Id == id);
+    }
+
+    public bool Update(T entity)
+    {
+        if (entity is null)
+            return false;
+
+        _context.Set<T>().Update(entity);
+        return Save();
+    }
+
+    protected bool Save()
+    {
+        try
+        {
+            _context.SaveChanges();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
