a47b8ef2ad3e0921d6b82f391ebd8a504ffb12f2
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 19:40:57 +0200

    Create IDepartmentRepository

---
+++ EmployeeManagementApp/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Interfaces;
+
+public interface IDepartmentRepository
+{
+    public ICollection<Department> GetDepartments();
+    public Department? GetDepartment(int id);
+    public bool CreateDepartment(Department department);
+    public bool UpdateDepartment(Department department);
+    public bool DeleteDepartment(int id);
+    public bool DepartmentExists(int id);
+}
