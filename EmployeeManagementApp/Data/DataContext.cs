84e0a0dff8663f6355fafad542624de425a6aa59
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 19:30:53 +0200

    Create DataContext

---
+++ EmployeeManagementApp/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementAPI.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Department> Departments { get; set; }
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+}
