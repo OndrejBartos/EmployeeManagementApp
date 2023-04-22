40f183c101af18f223bf709d2b249048f4b37c28
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 20:38:15 +0200

    Add dependency injection for repositories

--- EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/Program.cs
@@ -1,8 +1,16 @@
+using EmployeeManagementAPI.Interfaces;
+using EmployeeManagementAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Dependency injection.
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
