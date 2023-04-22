89f551a6679c53acab0c7b0b52014c9ebff61abc
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 20:27:09 +0200

    Make IEmployeeRepository and IDepartmentRepository inherit from IBaseRepository

--- EmployeeManagementApp/Interfaces/IDepartmentRepository.cs
+++ EmployeeManagementApp/Interfaces/IDepartmentRepository.cs
@@ -2,12 +2,4 @@
 
 namespace EmployeeManagementAPI.Interfaces;
 
-public interface IDepartmentRepository
-{
-    public ICollection<Department> GetDepartments();
-    public Department? GetDepartment(int id);
-    public bool CreateDepartment(Department department);
-    public bool UpdateDepartment(Department department);
-    public bool DeleteDepartment(int id);
-    public bool DepartmentExists(int id);
-}
+public interface IDepartmentRepository : IBaseRepository<Department> { }
--- EmployeeManagementApp/Interfaces/IEmployeeRepository.cs
+++ EmployeeManagementApp/Interfaces/IEmployeeRepository.cs
@@ -2,12 +2,4 @@
 
 namespace EmployeeManagementAPI.Interfaces;
 
-public interface IEmployeeRepository
-{
-    public ICollection<Employee> GetEmployees();
-    public Employee? GetEmployee(int id);
-    public bool CreateEmployee(Employee employee);
-    public bool UpdateEmployee(Employee employee);
-    public bool DeleteEmployee(int id);
-    public bool EmployeeExists(int id);
-}
+public interface IEmployeeRepository : IBaseRepository<Employee> { }
