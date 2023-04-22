b3f391a64d1df2cacea3cf39a5deadec07fdad32
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 20:36:12 +0200

    Create EmployeeRepository and DepartmentRepository

---
+++ EmployeeManagementApp/Repositories/DepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagementAPI.Data;
+using EmployeeManagementAPI.Interfaces;
+using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Repositories;
+
+public class DepartmentRepository : BaseRepository<Department>, IDepartmentRepository
+{
+    public DepartmentRepository(DataContext context) : base(context) { }
+}
---
+++ EmployeeManagementApp/Repositories/EmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagementAPI.Data;
+using EmployeeManagementAPI.Interfaces;
+using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Repositories;
+
+public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
+{
+    public EmployeeRepository(DataContext context) : base(context) { }
+}
