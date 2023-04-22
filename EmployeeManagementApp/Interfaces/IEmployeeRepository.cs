0c9dc9dc9de0112ca7717f946df603817d3614e4
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-22 19:38:36 +0200

    Create IEmployeeRepository

---
+++ EmployeeManagementApp/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Interfaces;
+
+public interface IEmployeeRepository
+{
+    public ICollection<Employee> GetEmployees();
+    public Employee? GetEmployee(int id);
+    public bool CreateEmployee(Employee employee);
+    public bool UpdateEmployee(Employee employee);
+    public bool DeleteEmployee(int id);
+    public bool EmployeeExists(int id);
+}
