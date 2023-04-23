5d9e9dcb37602acc411ba1a12f42ee87dc48ae5f
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-04-23 11:13:45 +0200

    Add employee DTO

---
+++ EmployeeManagementApp/Dto/EmployeeDto.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementAPI.Interfaces;
+using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Dto;
+
+public class EmployeeDto : IModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int Salary { get; set; }
+    public int DepartmentId { get; set; }
+}
