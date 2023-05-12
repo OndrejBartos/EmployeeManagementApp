98b2ab149ba56799f72703b5bda2bde6aab470a0
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-05-12 14:51:53 +0200

    Add Administrators DbSet

--- EmployeeManagementApp/Data/DataContext.cs
+++ EmployeeManagementApp/Data/DataContext.cs
@@ -7,6 +7,7 @@ public class DataContext : DbContext
 {
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Department> Departments { get; set; }
+    public DbSet<Administrator> Administrators { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 }
