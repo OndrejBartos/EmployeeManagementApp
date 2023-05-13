f0da482d1b7966f2559d92466bde56feec785a36
OndrejBartos <bartos.ondrej.business@gmail.com>
2023-05-13 17:32:43 +0200

    Create WPF project and create login window

---
+++ EmployeeManagementWPF/Services/ApiService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EmployeeManagementWPF.Services;
+
+public class ApiService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _baseUrl;
+    private string? _token;
+
+    public bool IsLoggedIn => _token is not null;
+
+    private class LoginResponse
+    {
+        public string Token { get; set; } = null!;
+    }
+
+    public ApiService(string baseUrl)
+    {
+        _httpClient = new HttpClient();
+        _baseUrl = baseUrl;
+    }
+
+    public async Task<bool> Login(string username, string password)
+    {
+        var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}/api/identity", new
+        {
+            username = username,
+            password = password
+        });
+
+        var content = await response.Content.ReadFromJsonAsync<LoginResponse>();
+        _token = content?.Token;
+
+        return _token is not null;
+    }
+}
---
+++ EmployeeManagementWPF/View/UserControls/CustomTextBox.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeeManagementWPF.View.UserControls;
+
+public partial class CustomTextBox : UserControl, INotifyPropertyChanged
+{
+    public CustomTextBox()
+    {
+        DataContext = this;
+        InitializeComponent();
+    }
+
+    private string? _label;
+    private bool _isPassword;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string Text => _isPassword ? passwordBox.Password : textBox.Text;
+
+    public string? Label
+    {
+        get { return _label; }
+        set 
+        { 
+            _label = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsPassword
+    {
+        get { return _isPassword; }
+        set 
+        {
+            _isPassword = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? caller = null)
+    {
+        if (caller is null)
+            return;
+
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+    }
+}
